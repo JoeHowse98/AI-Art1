eae951e46459f9a8983e233c9014c5c7715e418f
Sammy <samsimss98@gmail.com>
2020-10-27 23:52:26 +0000

    Refactor / Clean

--- static/sketch.js
+++ static/sketch.js
@@ -9,54 +9,37 @@ var randH = 0;
 
 // New function called preload - called before setup to ensure strings are read from textfile
 function preload() {
-  adjectives = loadStrings('static/strings/adjectives.txt'); //loads contents of text file into adjectives array
-  nouns = loadStrings('static/strings/nouns.txt'); //loads contents of text file into adjectives array
-  verbs = loadStrings('static/strings/verbs.txt'); //loads contents of text file into adjectives array
+    adjectives = loadStrings('static/strings/adjectives.txt'); //loads contents of text file into adjectives array
+    nouns = loadStrings('static/strings/nouns.txt'); //loads contents of text file into nouns array
+    verbs = loadStrings('static/strings/verbs.txt'); //loads contents of text file into verbs array
 }
 
 function setup() {
-
-  colorMode(HSB);
-
-
-  randH = random(361)
-  console.log(randH);
-
-  createCanvas(2000, 2000);
-  background(10);
-  
+    colorMode(HSB);
+    randH = random(361);
+    createCanvas(2000, 2000);
+    background(10);
     var rnd_adjective = random(adjectives);
     var rnd_noun = random(nouns);
     var rnd_verb = random(verbs);
 
-  
-  textSize(75)
-  
-  fill (0, 0, 100);
-  
-  text("the", 50, 100);
-  
-  text(rnd_adjective, 50, 200)
-  
-  text(rnd_noun, 50, 300)
-  
-  text(rnd_verb, 50, 400)
+    textSize(75)
+    fill(0, 0, 100);
+    text("the", 50, 100);
+    text(rnd_adjective, 50, 200);
+    text(rnd_noun, 50, 300);
+    text(rnd_verb, 50, 400)
 }
 
-function draw () {
-  
-  var x = map(noise (xoff), 0, 1, 0, 2100);
-  var y = map(noise (yoff), 0, 1, 0, 2100);
+function draw() {
+    var x = map(noise(xoff), 0, 1, 0, 2100);
+    var y = map(noise(yoff), 0, 1, 0, 2100);
 
-  //var randH = random(0, 360)
+    xoff += 0.001;
+    yoff += 0.001;
 
-  xoff += 0.001
-  yoff += 0.001
-  
-  noStroke();
-  strokeCap(SQUARE)
-  fill(randH, 100, 100, 0.1);
-  ellipse (x, y, 5, 350);
-  //Loop()
-   
+    noStroke();
+    strokeCap(SQUARE);
+    fill(randH, 100, 100, 0.1);
+    ellipse(x, y, 5, 350);
 }
